4ef4540f966616876de58db8fff880fb050d420d
Dipesh B C <bcdipeshwork@gmail.com>
2024-01-13 12:24:20 -0500

    Refactor bubble sort

--- bubble.js
+++ bubble.js
@@ -1,12 +1,12 @@
 function bubbleSort(arr) {
-  // Loop over all the elements of the array
-  for (let i = 0; i < arr.length; i++) {
-    // Flag to keep track of whether a swap took place
+  // loop over all the elements of the array
+  for (let i = 0; i < arr.length; i += 1) {
+    // flag to keep track of whether a swap took place
     let lastSwapped = false;
-    // Loop only upto elements that are unsorted
-    for (let j = 0; j < arr.length - i; j++) {
-      // If current element is greater that the next element
-      // Swap places
+
+    // loop only upto elements that are unsorted
+    for (let j = 0; j < arr.length - i; j += 1) {
+      // if current element is greater that the next element, swap places
       if (arr[j] > arr[j + 1]) {
         const temp = arr[j];
         arr[j] = arr[j + 1];
@@ -14,8 +14,8 @@ function bubbleSort(arr) {
         lastSwapped = true;
       }
     }
-    // If there was no swap break the loop because
-    // the array is sorted
+
+    // if there was no swap break the loop because the array is sorted
     if (!lastSwapped) break;
   }
 
